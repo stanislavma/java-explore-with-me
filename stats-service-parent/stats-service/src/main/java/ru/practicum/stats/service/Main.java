7cc64d06d4b7bcf5fabdaa4fd7e9c096dd5b0ef4
smdev <msvdev@yandex.com>
2024-07-29 00:34:41 +0500

    feature: project structure done, add and get stats

--- stats-service-parent/stats-service/src/main/java/ru/practicum/stats/service/Main.java
+++ stats-service-parent/stats-service/src/main/java/ru/practicum/stats/service/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsService {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StatsService.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
