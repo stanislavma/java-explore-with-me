707870cd4f610920cdd0dbb2c10e3e4235a23885
smdev <msvdev@yandex.com>
2024-07-29 05:43:08 +0500

    fix: fix sorting

--- stats-service-parent/stats-service/src/main/java/ru/practicum/stats/service/StatsRepository.java
+++ stats-service-parent/stats-service/src/main/java/ru/practicum/stats/service/StatsRepository.java
@@ -13,22 +13,26 @@ public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("SELECT new ru.practicum.stats.service.model.EndpointHit(s.app, s.uri, COUNT(s)) " +
             "FROM EndpointHit s WHERE s.hitDate BETWEEN :start AND :end " +
-            "GROUP BY s.app, s.uri")
+            "GROUP BY s.app, s.uri " +
+            "ORDER BY COUNT(s) DESC")
     List<EndpointHit> findStatsWithoutUris(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.stats.service.model.EndpointHit(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM EndpointHit s WHERE s.hitDate BETWEEN :start AND :end " +
-            "GROUP BY s.app, s.uri")
+            "GROUP BY s.app, s.uri " +
+            "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<EndpointHit> findUniqueStatsWithoutUris(LocalDateTime start, LocalDateTime end);
 
     @Query("SELECT new ru.practicum.stats.service.model.EndpointHit(s.app, s.uri, COUNT(s.id)) " +
             "FROM EndpointHit s WHERE s.hitDate BETWEEN :start AND :end " +
-            "AND s.uri IN :uris GROUP BY s.app, s.uri")
+            "AND s.uri IN :uris GROUP BY s.app, s.uri " +
+            "ORDER BY COUNT(s.id) DESC")
     List<EndpointHit> findStats(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.stats.service.model.EndpointHit(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM EndpointHit s WHERE s.hitDate BETWEEN :start AND :end " +
-            "AND s.uri IN :uris GROUP BY s.app, s.uri")
+            "AND s.uri IN :uris GROUP BY s.app, s.uri " +
+            "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<EndpointHit> findUniqueStats(LocalDateTime start, LocalDateTime end, List<String> uris);
 
 }
